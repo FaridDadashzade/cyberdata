c5f7fa92ea9b2a36350d8e7f396a5374f802cad1
FVREED <73734920+FaridDadashzade@users.noreply.github.com>
2021-12-03 22:43:13 +0400

    Update cyber.py

--- cyber.py
+++ cyber.py
@@ -24,10 +24,10 @@ print(ağ + "1. Telethon\n2. Pyrogram")
 seçim = int(input(ağ + "\nBir seçim edin: "))
 
 if ( seçim==1 ):
-  os.system("")
+  os.system("git clone https://github.com/FaridDadashzade/cyberdata && cd cyberdata && pip install -r requirements.txt && python telethon.py")
 
 if ( seçim==2 ):
-  os.system("")
+  os.system("git clone https://github.com/FaridDadashzade/cyberdata && cd cyberdata && pip install -r requirements.txt && python pyrogram.py")
   
 else:
 	print("\nYanlış seçim...")
